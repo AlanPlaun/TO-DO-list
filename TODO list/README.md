3c417ef66e7170c80bd0c8711dbccc943ed2e0a1
ALAN PLAUN <47165921@ort>
2023-03-27 10:37:44 -0300

    Falta borrar fecha anterior

---
+++ TODO list/README.md
@@ -0,0 +1,23 @@
+# TO-DO-list
+
+El objetivo de este TP es construir nuestra propia TODO List, usando lo aprendido de Javascript: arrays, objetos, condicionales, repetitivas, variables y manejo de DOM.
+
+- Cada vez que se agregue un nuevo elemento, la lista lo deberá mostrar y, en caso que se marque como realizado deberá mostrarse tachado.             ❌
+
+- Es requisito usar un array para almacenar los TODOs y Bootstrap.                      FALTA BOOSTRAP
+
+- Cada elemento tendrá además la fecha y hora (timestamp) de cuando un elemento fue creado y cuándo fue tachado.             FALTA QUE SE BORRE LA FECHA ANTERIOR
+
+- Agregar otro botón que muestre qué tarea fue la más rápido en realizarse. 
+
+Entonces, se mostrara el input para agregar el elemento, y su boton para hacerlo, donde se valide que se haya ingresado correctamente. El elemento de la lista tendra la opcion de marcarlo como hecho, con un checkbox, y por ultimo, un boton que te muestre cual fue la opcion mas rapida en realizarse.
+
+Opcionales plus:
+
+- Agregar un botón para eliminar un TODO y crear uno nuevo.             ❌
+
+- Agregar un botón sobre cada item de la lista, donde se permita eliminarlo.             ❌
+
+Entregar el tp en un repositorio de Github. El mismo se hace de a 2.
+
+Cualquier cosa estoy a disposición
--- TODO list/index.js
+++ TODO list/index.js
@@ -1,55 +1,73 @@
-// Create a "close" button and append it to each list item
+//pone los elementos ya ingresados
 var myNodelist = document.getElementsByTagName("LI");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
+    var hora = new Date(Date.now());
+    var txthora = document.createTextNode(hora.getHours() + ":" + hora.getMinutes() + ", " + hora.toDateString());
+    var p = document.createElement("p");
+    p.className = "hora";
+    p.appendChild(txthora);
+    myNodelist[i].appendChild(p)
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    myNodelist[i].appendChild(span);
 }
 
-// Click on a close button to hide the current list item
+// borra
 var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
+    close[i].onclick = function () {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
 }
-
-// Add a "checked" symbol when clicking on a list item
+//cambia estado a checked
 var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
+var horaDeTerminación = new Date(Date.now());
+list.addEventListener('click', function (ev) {
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+        txthora = document.createTextNode(horaDeTerminación.getHours() + ":" + horaDeTerminación.getMinutes() + ", " + horaDeTerminación.toDateString());
+        p.appendChild(txthora);
+        ev.target.appendChild(p)
+    }
 }, false);
 
-// Create a new list item when clicking on the "Add" button
+//crea elemento y chequea que se haya ingresado
 function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("Tenes que escribir algo!!!!!!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
-
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+        alert("Tenes que escribir algo!!!!!!");
+    } else {
+        document.getElementById("myUL").appendChild(li);
     }
-  }
+    document.getElementById("myInput").value = "";
+
+    var hora = new Date(Date.now());
+    var txthora = document.createTextNode(hora.getHours() + ":" + hora.getMinutes() + ", " + hora.toDateString());
+    var p = document.createElement("p");
+    p.className = "hora";
+    p.appendChild(txthora);
+    li.appendChild(p)
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function () {
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+
 }
